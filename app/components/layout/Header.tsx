1acdcca624fc6e3c9369cff64790216e83d0ebae
BenoitPrmt <benoitparmentier45@gmail.com>
2025-02-16 15:04:32 +0100

    feat(header): added link on logo

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import AboutModal from "~/components/modal/AboutModal";
+import {Link} from "react-router";
 
 const Header = () => {
     return (
         <nav className="bg-gray-800 p-4 text-white flex justify-between items-center w-full">
 
-            <div className="flex items-center space-x-2">
-                <img src="/favicon.ico" alt="logo" className="w-8 h-8" />
-                <h1 className="font-bold text-2xl bg-gradient-to-r from-[#f17587] via-[#f68058] to-[#fe901a] text-transparent bg-clip-text">Check & Shop</h1>
-            </div>
+            <Link to="/">
+                <div className="flex items-center space-x-2">
+                    <img src="/favicon.ico" alt="logo" className="w-8 h-8" />
+                    <h1 className="font-bold text-2xl bg-gradient-to-r from-[#f17587] via-[#f68058] to-[#fe901a] text-transparent bg-clip-text">Check & Shop</h1>
+                </div>
+            </Link>
 
             <AboutModal />
         </nav>
